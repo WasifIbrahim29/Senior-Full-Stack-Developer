3e85e8db4b6e6c3c13fde1b1b5eaf869666297f0
Hamza Yaseen <hamzza.yaseen@cooperativecomputing.com>
2024-01-08 15:57:26 +0500

    analog clock

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [rotation, setRotation] = useState(0)
+
+  const [seconds, setSeconds] = useState(0)
+
+  useEffect(() => {
+    console.log('RENDERED')
+    console.log({ rotation })
+    setTimeout(() => {
+      setSeconds((seconds) => seconds + 1)
+      const interval = setInterval(() => {
+        setRotation((rotation) => {
+          if (rotation >= 360) {
+            return 0
+          }
+          return rotation + 6
+        })
+      }, 1000)
+      return () => clearInterval(interval)
+    }, 1000)
+  }, [seconds])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span className="seconds" style={{ transform: `rotate(${rotation}deg)` }}>
+        <img src="hour.png" />
+      </span>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
